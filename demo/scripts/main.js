1ddf69c991d26391eea6b72f85ddec4432ca2e51
Kimmo Brunfeldt <kimmobrunfeldt@gmail.com>
2014-10-06 01:35:10 +0300

    Work demo page layout forward

--- demo/scripts/main.js
+++ demo/scripts/main.js
@@ -2,9 +2,9 @@ function randInt(min, max) {
     return Math.floor(Math.random()*(max-min+1)+min);
 }
 
-var circle = new ProgressBar.Circle('#circle-progress', {
+var circle = new ProgressBar.Circle('#landing-progress', {
     color: "#6FD57F",
-    strokeWidth: 1,
+    strokeWidth: 0.3,
     trailColor: null
 });
 
