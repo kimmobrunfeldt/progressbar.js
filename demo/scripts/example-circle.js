d6ea5040267cf9015b398eaab531c12c944184a2
Kimmo Brunfeldt <kimmobrunfeldt@gmail.com>
2014-10-10 00:51:34 +0300

    Add small paragraph for examples

--- demo/scripts/example-circle.js
+++ demo/scripts/example-circle.js
@@ -1,7 +1,7 @@
 var element = document.getElementById('example-1-container');
 var circle = new ProgressBar.Circle(element, {
     color: "#FCB03C",
-    strokeWidth: 1.5,
+    strokeWidth: 1,
     trailColor: "#ccc",
     fill: "#FFF9F0"
 });
