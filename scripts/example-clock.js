74c1b49637009e56d6d9504772650ed9391c3d0b
Kimmo Brunfeldt <kimmobrunfeldt@gmail.com>
2014-10-13 22:32:03 +0300

    Add clock example

---
+++ scripts/example-clock.js
@@ -0,0 +1,16 @@
+var element = document.getElementById('example-3-container');
+element.innerHTML = '<header id="clock-seconds"></header>';
+var textElement = document.getElementById('clock-seconds');
+
+var seconds = new ProgressBar.Circle(element, {
+    duration: 200,
+    color: "#FCB03C",
+    trailColor: "#ddd"
+});
+
+setInterval(function() {
+    var second = new Date().getSeconds();
+    seconds.animate(second / 60, function() {
+        textElement.innerHTML = second;
+    });
+}, 1000);
