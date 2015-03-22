0c36cb788d7c37723e8745c29c6fd82acb66f5dd
Ant Cunningham <ant.cunningham@gmail.com>
2015-03-22 17:56:28 -0400

    Coding standards.

--- test/path-behaviour.js
+++ test/path-behaviour.js
@@ -13,60 +13,60 @@ var PRECISION = 2;
 var svgStr = '<svg version="1.1" id="progress-bar" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 197 165.39555" enable-background="0 0 197 165.39555" xml:space="preserve"><path id="progress-path" fill="none" stroke="none" stroke-width="15" stroke-miterlimit="10" d="m 31.7,160.3 c -15,-16.2 -24.2,-38 -24.2,-61.8 0,-50.3 40.7,-91 91,-91 50.3,0 91,40.7 91,91 0,23.9 -9.2,45.6 -24.2,61.8"/></svg>';
 
 var ANIM_PROP = {
-	'styleName': 'stroke-offset',
-	'scriptName': 'strokeOffset'
+    'styleName': 'stroke-offset',
+    'scriptName': 'strokeOffset'
 };
 
 var barOpts = {
-	duration: 800,
+    duration: 800,
     from: {strokeOffset: 0},
     to: { strokeOffset: 0 },
     step: function (state, self, attachment) {
-    	attachment.setAttribute(ANIM_PROP.scriptName, state[ANIM_PROP.scriptName]);
+        attachment.setAttribute(ANIM_PROP.scriptName, state[ANIM_PROP.scriptName]);
     }
 };
 
 var createPath = function () {
-	var container = document.getElementById('container'),
-	    svg = document.createElementNS('http://www.w3.org/2000/svg', 'svg'),
-	    path = document.createElementNS('http://www.w3.org/2000/svg', 'path');
+    var container = document.getElementById('container'),
+        svg = document.createElementNS('http://www.w3.org/2000/svg', 'svg'),
+        path = document.createElementNS('http://www.w3.org/2000/svg', 'path');
 
     container.setAttribute('style', 'width: 200px;');
 
-	svg.setAttribute('version', '1.1');
-	svg.setAttribute('id', 'progress-bar');
-	svg.setAttribute('x', '0px');
-	svg.setAttribute('y', '0px');
-	svg.setAttribute('viewBox', '0 0 197 165.39555');
-	svg.setAttribute('enable-background', '0 0 197 165.39555');
+    svg.setAttribute('version', '1.1');
+    svg.setAttribute('id', 'progress-bar');
+    svg.setAttribute('x', '0px');
+    svg.setAttribute('y', '0px');
+    svg.setAttribute('viewBox', '0 0 197 165.39555');
+    svg.setAttribute('enable-background', '0 0 197 165.39555');
 
-	var attrs = {
-		"id": "progress-path",
-		"fill": "none",
-		"stroke": "#ccc",
-		"stroke-width":"15",
-		"stroke-miterlimit": "10",
-		"d": "m 31.7,160.3 c -15,-16.2 -24.2,-38 -24.2,-61.8 0,-50.3 40.7,-91 91,-91 50.3,0 91,40.7 91,91 0,23.9 -9.2,45.6 -24.2,61.8"
-	};
+    var attrs = {
+        "id": "progress-path",
+        "fill": "none",
+        "stroke": "#ccc",
+        "stroke-width":"15",
+        "stroke-miterlimit": "10",
+        "d": "m 31.7,160.3 c -15,-16.2 -24.2,-38 -24.2,-61.8 0,-50.3 40.7,-91 91,-91 50.3,0 91,40.7 91,91 0,23.9 -9.2,45.6 -24.2,61.8"
+    };
 
-	for (var i in attrs) {
-		path.setAttribute(i, attrs[i]);
-	}
+    for (var i in attrs) {
+        path.setAttribute(i, attrs[i]);
+    }
 
-	svg.appendChild(path);
+    svg.appendChild(path);
 
-	container.appendChild(svg);
+    container.appendChild(svg);
 
-	return document.getElementById('progress-path');
+    return document.getElementById('progress-path');
 };
 
 var destroyPath = function () {
-	var container = document.getElementById('container'),
-		svg = document.getElementById('progress-bar');
-	container.removeAttribute('style');
+    var container = document.getElementById('container'),
+        svg = document.getElementById('progress-bar');
+    container.removeAttribute('style');
 
-	container.removeChild(svg);
-	container.innerHTML = '';
+    container.removeChild(svg);
+    container.innerHTML = '';
 };
 
 var afterEachCase = function() {
@@ -80,21 +80,21 @@ var afterEachCase = function() {
 
 var pathTests = function pathTests () {
 
-	it('step function should recieve a reference to ProgressBar as argument #2', function () {
-		this.bar.animate(1, {duration: 500});
+    it('step function should recieve a reference to ProgressBar as argument #2', function () {
+        this.bar.animate(1, {duration: 500});
 
-		// we only care about the second arg, for each call so we need to manually
-		// inspect them since we dont know what state would look like
-		var ok = true;
+        // we only care about the second arg, for each call so we need to manually
+        // inspect them since we dont know what state would look like
+        var ok = true;
 
-		for (var i =0; i < this.step.args.length; i++) {
-			if (this.step.args[i][1] !== this.bar) {
-				ok = false;
-			}
-		}
+        for (var i =0; i < this.step.args.length; i++) {
+            if (this.step.args[i][1] !== this.bar) {
+                ok = false;
+            }
+        }
 
-		expect(ok).to.be.true();
-	});
+        expect(ok).to.be.true();
+    });
 
     it('step function should recieve a reference to ProgressBar as argument #3', function () {
         this.bar.animate(1, {duration: 500});
@@ -163,9 +163,9 @@ var pathTests = function pathTests () {
 };
 
 module.exports = {
-	options: barOpts,
-	runTests: pathTests,
-	afterEachCase: afterEachCase,
-	createPath: createPath,
-	destroyPath: destroyPath
+    options: barOpts,
+    runTests: pathTests,
+    afterEachCase: afterEachCase,
+    createPath: createPath,
+    destroyPath: destroyPath
 };
