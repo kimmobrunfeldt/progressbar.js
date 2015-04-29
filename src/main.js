ffb151d954d7486a96c2b8ed05a4648a1613f206
Kimmo Brunfeldt <kimmobrunfeldt@gmail.com>
2015-04-30 00:02:19 +0300

    Expose more internal modules for convenience

--- src/main.js
+++ src/main.js
@@ -1,15 +1,17 @@
-// Different shaped progress bars
-var Line = require('./line');
-var Circle = require('./circle');
-var SemiCircle = require('./semicircle');
-
-// Lower level API to use any SVG path
-var Path = require('./path');
-
-
 module.exports = {
-    Line: Line,
-    Circle: Circle,
-    SemiCircle: SemiCircle,
-    Path: Path
+    // Higher level API, different shaped progress bars
+    Line: require('./line'),
+    Circle: require('./circle'),
+    SemiCircle: require('./semicircle'),
+
+    // Lower level API to use any SVG path
+    Path: require('./path'),
+
+    // Base-class for creating new custom shapes
+    // to be in line with the API of built-in shapes
+    // Undocumented.
+    Shape: require('./shape'),
+
+    // Internal utils, undocumented.
+    utils: require('./utils')
 };
