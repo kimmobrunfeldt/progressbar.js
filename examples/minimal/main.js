e53d59788d1b0d571587d6870623715a284d7554
Kimmo Brunfeldt <kimmobrunfeldt@gmail.com>
2016-03-01 23:46:47 +0200

    Delete old page

--- examples/minimal/main.js
+++
@@ -1,9 +0,0 @@
-window.onload = function onLoad() {
-    var circle = new ProgressBar.Circle('#progress', {
-        color: '#FCB03C',
-        duration: 3000,
-        easing: 'easeInOut'
-    });
-
-    circle.animate(1);
-};
