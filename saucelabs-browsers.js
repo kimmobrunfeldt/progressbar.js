02a322d778c85e42518dea03973f5210f819c2f9
Kimmo Brunfeldt <kimmobrunfeldt@gmail.com>
2017-09-10 00:37:33 +0300

    Update saucelabs browsers

--- saucelabs-browsers.js
+++ saucelabs-browsers.js
@@ -4,66 +4,59 @@
 module.exports = {
 
     // Chrome
-    sauce_linux_chrome_47: {
+    sauce_linux_chrome_60: {
         base: 'SauceLabs',
         browserName: 'chrome',
         platform: 'linux',
-        version: '47'
+        version: '60'
     },
-    sauce_windows_chrome_47: {
+    sauce_windows_chrome_60: {
         base: 'SauceLabs',
         browserName: 'chrome',
         platform: 'Windows 8.1',
-        version: '47'
+        version: '60'
     },
-    sauce_osx_chrome_47: {
+    sauce_osx_chrome_60: {
         base: 'SauceLabs',
         browserName: 'chrome',
         platform: 'OS X 10.9',
-        version: '47'
+        version: '60'
     },
 
     // Safari
-    sauce_osx_safari9: {
+    sauce_osx_safari10: {
         base: 'SauceLabs',
         browserName: 'safari',
-        platform: 'OS X 10.11',
-        version: '9'
-    },
-    sauce_osx_safari8: {
-        base: 'SauceLabs',
-        browserName: 'safari',
-        platform: 'OS X 10.10',
-        version: '8'
+        platform: 'OS X 10.12',
+        version: '10'
     },
 
     // Firefox
-
     sauce_osx_firefox_beta: {
         base: 'SauceLabs',
         browserName: 'firefox',
         platform: 'OS X 10.11',
         version: 'beta'
     },
-    sauce_osx_firefox_43: {
+    sauce_osx_firefox_55: {
         base: 'SauceLabs',
         browserName: 'firefox',
         platform: 'OS X 10.9',
-        version: '43'
+        version: '55'
     },
 
-    sauce_linux_firefox_43: {
+    sauce_linux_firefox_55: {
         base: 'SauceLabs',
         browserName: 'firefox',
         platform: 'linux',
-        version: '43'
+        version: '55'
     },
 
-    sauce_windows_firefox_43: {
+    sauce_windows_firefox_55: {
         base: 'SauceLabs',
         browserName: 'firefox',
         platform: 'Windows 8.1',
-        version: '43'
+        version: '55'
     },
 
     // Opera
@@ -78,52 +71,21 @@ module.exports = {
     sauce_windows_ie_11: {
         base: 'SauceLabs',
         browserName: 'internet explorer',
-        platform: 'Windows 8.1',
+        platform: 'Windows 10',
         version: '11'
     },
     sauce_windows_ie_10: {
         base: 'SauceLabs',
         browserName: 'internet explorer',
-        platform: 'Windows 7',
+        platform: 'Windows 8',
         version: '10'
-    },
-    sauce_windows_ie_9: {
-        base: 'SauceLabs',
-        browserName: 'internet explorer',
-        platform: 'Windows 7',
-        version: '9'
     },
 
     // MS Edge
-
     sauce_windows_edge: {
         base: 'SauceLabs',
         browserName: 'MicrosoftEdge',
         platform: 'Windows 10',
         version: '20.10240'
     },
-
-    // Mobile
-
-    // Android
-    sauce_android: {
-        base: 'SauceLabs',
-        browserName: 'android',
-        platform: 'Linux',
-        version: '4.4'
-    },
-
-    // iOS iPhone
-    sauce_ios9_iphone: {
-        base: 'SauceLabs',
-        browserName: 'iphone',
-        platform: 'OS X 10.10',
-        version: '9.2'
-    },
-    sauce_ios8_iphone: {
-        base: 'SauceLabs',
-        browserName: 'iphone',
-        platform: 'OS X 10.10',
-        version: '8.4'
-    }
 };
