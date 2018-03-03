8fca8dbb36f91febd24ccec5d4addb45bb171c31
Anudeep Bulla <anudeep.3998@yahoo.com>
2018-03-02 23:53:53 -0500

    remove comment

--- src/line.js
+++ src/line.js
@@ -19,7 +19,6 @@ Line.prototype.constructor = Line;
 
 Line.prototype._initializeSvg = function _initializeSvg(svg, opts) {
     svg.setAttribute('viewBox', '0 0 100 ' + opts.strokeWidth);
-    //svg.setAttribute('preserveAspectRatio', 'none');
 };
 
 Line.prototype._pathString = function _pathString(opts) {
